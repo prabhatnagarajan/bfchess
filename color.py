af28e69e2d6f315a4406982790d0500b01a77249
Neil Fulwiler <neil.fulwiler@gmail.com>
2017-07-10 21:42:37 -0400

    add --cli/--latex for switching between cli quiz and latex output

--- color.py
+++ color.py
@@ -1,4 +1,16 @@
 from enum import Enum
 class Color(Enum):
-	WHITE = 1
-	BLACK = 2
+    WHITE = 1
+    BLACK = 2
+
+    def __str__(self):
+        if self is Color.WHITE:
+            return 'W'
+        else:
+            return 'B'
+
+    def __eq__(self, o):
+        if self is o:
+            return True
+        else:
+            return str(self) == o
--- sq_color_quiz.py
+++ sq_color_quiz.py
@@ -1,41 +1,86 @@
 from constants import *
 from color import *
+import argparse
 import numpy as np
+import random
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    quiz_type = parser.add_mutually_exclusive_group(required=True)
+    quiz_type.add_argument('--cli', action='store_true')
+    quiz_type.add_argument('--latex', action='store_true')
+    return parser.parse_args()
+
 
 def generate_quiz():
-	#Quiz is dictionary mapping a problem number to square
-	sequence = np.random.permutation(np.array(range(NUM_SQUARES)))
-	print sequence
-	quiz = dict()
-	answer_key = dict()
-	for num in range(NUM_SQUARES):
-		quiz[num] = SQUARES[sequence[num]]
-		answer_key[num] = SQUARE_COLOR[SQUARES[sequence[num]]]
-	return (quiz, answer_key)
+    #Quiz is dictionary mapping a problem number to square
+    sequence = np.random.permutation(np.array(range(NUM_SQUARES)))
+    quiz = dict()
+    answer_key = dict()
+    for num in range(NUM_SQUARES):
+        quiz[num] = SQUARES[sequence[num]]
+        answer_key[num] = SQUARE_COLOR[SQUARES[sequence[num]]]
 
-quiz, answer_key = generate_quiz()
+    return (quiz, answer_key)
 
-quiz_latex =[]
-quiz_latex.append("\\subsection {Quiz}")
-quiz_latex.append("\\begin{enumerate}")
-for num in range(NUM_SQUARES):
-	item = "\t \\item "
-	item = item + quiz[num]
-	quiz_latex.append(item)
-quiz_latex.append("\\end{enumerate}")
 
-quiz_latex.append("\\newpage")
-quiz_latex.append("\\subsection{Answer Key}")
-quiz_latex.append("\\begin{enumerate}")
-for num in range(NUM_SQUARES):
-	item = "\t \\item "
-	item = item + quiz[num] + " "
-	if answer_key[num] == Color.BLACK:
-		item = item + "\\textbf{B}"
-	else:
-		item = item + "\\textbf{W}"
-	quiz_latex.append(item)
-quiz_latex.append("\\end{enumerate}")
+def generate_cli_quiz():
+    quiz, answer_key = generate_quiz()
+    questions = []
+    for num in range(NUM_SQUARES):
+        questions.append((quiz[num], answer_key[num]))
 
-for command in quiz_latex:
-	print command
+    wrong = questions[:]
+    while wrong:
+        questions = wrong[:]
+        random.shuffle(questions)
+        wrong = []
+        for q, a in questions:
+            guess = raw_input('%s: ' % q)
+            if guess == a:
+                print('correct')
+            else:
+                print('wrong. correct answer: "%s"' % a)
+                wrong.append((q, a))
+
+
+def generate_latex_quiz():
+    quiz, answer_key = generate_quiz()
+
+    quiz_latex =[]
+    quiz_latex.append("\\subsection {Quiz}")
+    quiz_latex.append("\\begin{enumerate}")
+    for num in range(NUM_SQUARES):
+        item = "\t \\item "
+        item = item + quiz[num]
+        quiz_latex.append(item)
+    quiz_latex.append("\\end{enumerate}")
+
+    quiz_latex.append("\\newpage")
+    quiz_latex.append("\\subsection{Answer Key}")
+    quiz_latex.append("\\begin{enumerate}")
+    for num in range(NUM_SQUARES):
+        item = "\t \\item "
+        item = item + quiz[num] + " "
+        if answer_key[num] == Color.BLACK:
+            item = item + "\\textbf{B}"
+        else:
+            item = item + "\\textbf{W}"
+        quiz_latex.append(item)
+    quiz_latex.append("\\end{enumerate}")
+
+    for command in quiz_latex:
+        print command
+
+
+def main():
+    args = parse_args()
+    if args.latex:
+        generate_latex_quiz()
+    else:
+        generate_cli_quiz()
+
+
+if __name__ == '__main__':
+    main()
